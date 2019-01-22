2e9e87e4d452b7fc25b56ee3fb926b752b37a21a
Franklin Chieze <chieze.franklin@gmail.com>
2019-01-22 21:40:28 +0100

    initial server code [2]

--- src/server.js
+++ src/server.js
@@ -11,6 +11,18 @@ app.get('/', async (req, res) => {
   res.status(200).send("Hello World!");
 });
 
+/** 
+ * The endpoint is POSTed to when certain events (like a user posted a message)
+ * occur.
+*/
+app.post('/message', async (req, res) => {
+  res.header('Content-Type', 'application/x-www-form-urlencoded');
+  // if Slack is "challenging" our URL in order to verify it
+  if (req.body.challenge) {
+    return res.status(200).json({ challenge: req.body.challenge });
+  }
+})
+
 let server = app.listen(process.env.PORT || 5000, () => {
   let port = server.address().port;
   console.log(`Server started on port ${port}`)
