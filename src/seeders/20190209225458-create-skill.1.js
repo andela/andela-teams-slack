ae64642731bf5316954758bbacb2c8144c56d387
Franklin Chieze <chieze.franklin@gmail.com>
2019-02-15 00:08:59 +0100

    add D0B skills to Quality

--- src/seeders/20190209225458-create-skill.1.js
+++ src/seeders/20190209225458-create-skill.1.js
@@ -35,6 +35,54 @@ module.exports = {
       id: 109,
       attributeId: 1,
       name: 'Version control'
+    }, {
+      id: 1095,
+      attributeId: 1,
+      name: ''
+    }, {
+      id: 110,
+      attributeId: 1,
+      name: 'Patterns (MVC)'
+    }, {
+      id: 111,
+      attributeId: 1,
+      name: '2 & 3 Tier Architecture'
+    }, {
+      id: 112,
+      attributeId: 1,
+      name: 'Security'
+    }, {
+      id: 113,
+      attributeId: 1,
+      name: 'Databases'
+    }, {
+      id: 114,
+      attributeId: 1,
+      name: 'APIs'
+    }, {
+      id: 115,
+      attributeId: 1,
+      name: 'UI/UX'
+    }, {
+      id: 116,
+      attributeId: 1,
+      name: 'End-to-End Testing'
+    }, {
+      id: 117,
+      attributeId: 1,
+      name: 'Debugging'
+    }, {
+      id: 118,
+      attributeId: 1,
+      name: 'Agile Process'
+    }, {
+      id: 119,
+      attributeId: 1,
+      name: 'Proper use of environments'
+    }, {
+      id: 120,
+      attributeId: 1,
+      name: 'Mobile Development'
     }], {}),
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Skills', null, {})
 };
