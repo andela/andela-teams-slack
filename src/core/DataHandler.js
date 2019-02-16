6b49a529d0144721739912a4fdfd8ad51d6e3c46
Franklin Chieze <chieze.franklin@gmail.com>
2019-02-16 03:12:34 +0100

    add array of dates to Feedback Analytics dialog [7]

--- src/core/DataHandler.js
+++ src/core/DataHandler.js
@@ -26,33 +26,47 @@ async function _getAttributesAndSkills() {
 }
 
 function _getDates() {
-  let dateMap = new Map();
+  let dateMap = {};//new Map();
+  let now = moment();
   for (let i = 0; i <= 90; i++) {
-    let date = moment().subtract(i, 'days');
+    let date = now.subtract(i, 'days');
     let monthName = date.format('MMMM');
-    if (dateMap.has(monthName)) {
-      console.log('dateMap.get(monthName)>>>>>>>>>>>>>>>>>>>>>>>>>');console.log(dateMap.get(monthName))
-      let dates = Array.from(dateMap.get(monthName));
-      console.log('typeof dates>>>>>>>>>>>>>>>>>>>>>>>>>');console.log(typeof dates)
+    if (dateMap[monthName]) { //dateMap.has(monthName)
+      console.log('dateMap.get(monthName)>>>>>>>>>>>>>>>>>>>>>>>>>');console.log(dateMap[monthName])
+      let dates = Array.from(dateMap[monthName]);
       console.log('dates>>>>>>>>>>>>>>>>>>>>>>>>>');console.log(dates)
-      dateMap.set(monthName, dates.push(date));
+      dateMap[monthName] = dates.push(date); //dateMap.set(monthName, dates.push(date));
     } else {
       console.log('set date>>>>>>>>>>>>>>>>>>>>>>>>>');console.log(date)
-      dateMap.set(monthName, [date]);
+      dateMap[monthName] = [date]; //dateMap.set(monthName, [date]);
+      console.log('dateMap>>>>>>>>>>>>>>>>>>>>>>>>>');console.log(dateMap)
     }
   }
   console.log('dateMap>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');console.log(dateMap)
   let option_groups = [];
-  for (let [monthName, dates] of dateMap) {
-    let group = {};
-    group.label = monthName;
-    group.options = dates.map(d => {
-      return {
-        label: d.format('dddd, MMMM Do YYYY'),
-        value: d.toDate()
-      }
-    });
-    option_groups.push(group);
+  // for (let [monthName, dates] of dateMap) {
+  //   let group = {};
+  //   group.label = monthName;
+  //   group.options = dates.map(d => {
+  //     return {
+  //       label: d.format('dddd, MMMM Do YYYY'),
+  //       value: d.toDate()
+  //     }
+  //   });
+  //   option_groups.push(group);
+  // }
+  for (let monthName in dateMap) {
+    if (dateMap.hasOwnProperty(monthName)) {
+      let group = {};
+      group.label = monthName;
+      group.options = dateMap[monthName].map(d => {
+        return {
+          label: d.format('dddd, MMMM Do YYYY'),
+          value: d.toDate()
+        }
+      });
+      option_groups.push(group);
+    }
   }
   console.log('option_groups>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');console.log(option_groups)
   return { option_groups };
