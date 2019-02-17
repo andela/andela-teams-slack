61fdd3e873c74997ccb7c0c09780d8cac0e16357
Franklin Chieze <chieze.franklin@gmail.com>
2019-02-17 03:22:59 +0100

    test attributes chart analytics [3]

--- src/core/AnalyticsController.js
+++ src/core/AnalyticsController.js
@@ -5,6 +5,39 @@ import Slack from '../integrations/Slack';
 
 const slack = new Slack();
 
+function _getAttributesChart(records) {
+  let attriGroupsMap = new Map();
+  let totalCount = 0;
+  for (let i = 0; i < records.length; i++) {
+    let record = records[i].get();console.log(record);
+    if (!record.skill) {console.log('no record.skill found')
+      continue;
+    }
+    record.skill = record.skill.get();console.log(record);
+    if (!record.skill.attribute) {console.log('no record.skill.attribute found')
+      continue;
+    }
+    record.skill.attribute = record.skill.attribute.get();console.log(record);
+    if (attriGroupsMap.has(record.skill.attribute)) {console.log(`${record.skill.attribute} is previously cached`);
+      attriGroupsMap.set(
+        record.skill.attribute,
+        Number(attriGroupsMap.get(record.skill.attribute)) + 1);
+    } else {console.log(`${record.skill.attribute} is not yet cached`);
+      attriGroupsMap.set(record.skill.attribute, 1);
+    }
+    totalCount += 1;
+  }
+  let rows = [];
+  for (let [attribute, count] of attriGroupsMap) {
+    rows.push({
+      attribute,
+      count,
+      percent: (Number(count) / totalCount) * 100
+    });
+  }
+  return rows;
+}
+
 async function _getFeedbackTable(records) {
   let resolvedUsersMap = new Map();
   let rows = [];
@@ -96,11 +129,7 @@ export default class AnalyticsController {
             attributes: ['name'],
           }]
         }];
-        query.attributes = [
-          'skill.attribute.name',
-          [models.sequelize.fn('count', models.sequelize.col('id')), 'count']
-        ];
-        query.group = ['skill.attribute.name'];
+        query.attributes = ['message'];
       }
       const records = await models.FeedbackInstance.findAll(query);
       records.forEach(r => console.log(r.get()));
@@ -109,6 +138,8 @@ export default class AnalyticsController {
         rows = await _getFeedbackTable(records);
       } else if (query.feedbackAnalyticsType === 'feedback_time_distribution') {
         rows = _getFeedbackTimeDistribution(records);
+      } else if (query.feedbackAnalyticsType === 'attributes_chart') {
+        rows = _getAttributesChart(records);
       }
       return res.status(200).json({ rows });
     } catch(error) {
