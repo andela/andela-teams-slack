0c16ff728762c4f632720d87e5a6f92ca466f6bc
Franklin Chieze <chieze.franklin@gmail.com>
2019-02-20 18:27:20 +0100

    change id of FeedbackInstance and Resource models to UUID [6]

--- src/seeders/20190220225458-create-resources.js
+++ src/seeders/20190220225458-create-resources.js
@@ -1,38 +1,38 @@
-const uuidv4 = require('uuid/v4');
+// const uuidv4 = require('uuid/v4');
 
-module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Resources', [{
-      id: uuidv4(),
-      url: 'https://www.pivotaltracker.com/projects/2313263',  
-      userId: 'U4R9RDUCQ'
-    }, {
-      id: uuidv4(),
-      url: 'https://www.pivotaltracker.com/projects/2313261',  
-      userId: 'U4R9RDUCQ'
-    }, {
-      id: uuidv4(),
-      url: 'https://github.com/andela/apollo-ah-backend',  
-      userId: 'U4R9RDUCQ'
-    }, {
-      id: uuidv4(),
-      url: 'https://github.com/andela/artemis-ah-backend',  
-      userId: 'U4R9RDUCQ'
-    }, {
-      id: uuidv4(),
-      url: 'https://github.com/andela/apollo-ah-frontend',  
-      userId: 'U4R9RDUCQ'
-    }, {
-      id: uuidv4(),
-      url: 'https://github.com/andela/artemis-ah-frontend',  
-      userId: 'U4R9RDUCQ'
-    }, {
-      id: uuidv4(),
-      url: 'https://www.pivotaltracker.com/n/projects/2313265',  
-      userId: 'UBP16GXMX'
-    }, {
-      id: uuidv4(),
-      url: 'https://www.pivotaltracker.com/n/projects/2313267',  
-      userId: 'UBP16GXMX'
-    }], {}),
-  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Skills', null, {})
-};
+// module.exports = {
+//   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Resources', [{
+//       id: uuidv4(),
+//       url: 'https://www.pivotaltracker.com/projects/2313263',  
+//       userId: 'U4R9RDUCQ'
+//     }, {
+//       id: uuidv4(),
+//       url: 'https://www.pivotaltracker.com/projects/2313261',  
+//       userId: 'U4R9RDUCQ'
+//     }, {
+//       id: uuidv4(),
+//       url: 'https://github.com/andela/apollo-ah-backend',  
+//       userId: 'U4R9RDUCQ'
+//     }, {
+//       id: uuidv4(),
+//       url: 'https://github.com/andela/artemis-ah-backend',  
+//       userId: 'U4R9RDUCQ'
+//     }, {
+//       id: uuidv4(),
+//       url: 'https://github.com/andela/apollo-ah-frontend',  
+//       userId: 'U4R9RDUCQ'
+//     }, {
+//       id: uuidv4(),
+//       url: 'https://github.com/andela/artemis-ah-frontend',  
+//       userId: 'U4R9RDUCQ'
+//     }, {
+//       id: uuidv4(),
+//       url: 'https://www.pivotaltracker.com/n/projects/2313265',  
+//       userId: 'UBP16GXMX'
+//     }, {
+//       id: uuidv4(),
+//       url: 'https://www.pivotaltracker.com/n/projects/2313267',  
+//       userId: 'UBP16GXMX'
+//     }], {}),
+//   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Skills', null, {})
+// };
