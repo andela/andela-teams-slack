bad17dc12e142a81701e26e9ed689b76871c4d91
Franklin Chieze <chieze.franklin@gmail.com>
2019-02-20 17:51:45 +0100

    change id of FeedbackInstance and Resource models to UUID [2]

--- src/migrations/20190209194158-create-resource.js
+++ src/migrations/20190209194158-create-resource.js
@@ -1,7 +1,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.createTable('Resources', {
       id: {
-        allowNull: false,
+        // allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4
