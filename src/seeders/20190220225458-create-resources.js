664d2ebfe49f0d12bd4a643629f26cd46fcc5d80
Franklin Chieze <chieze.franklin@gmail.com>
2019-02-20 18:00:02 +0100

    change id of FeedbackInstance and Resource models to UUID [4]

--- src/seeders/20190220225458-create-resources.js
+++ src/seeders/20190220225458-create-resources.js
@@ -1,34 +1,34 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Resources', [{
-      id: Sequelize.UUIDV4,
+      id: Sequelize.Utils.generateUUID(),
       url: 'https://www.pivotaltracker.com/projects/2313263',  
       userId: 'U4R9RDUCQ'
     }, {
-      id: Sequelize.UUIDV4,
+      id: Sequelize.Utils.generateUUID(),
       url: 'https://www.pivotaltracker.com/projects/2313261',  
       userId: 'U4R9RDUCQ'
     }, {
-      id: Sequelize.UUIDV4,
+      id: Sequelize.Utils.generateUUID(),
       url: 'https://github.com/andela/apollo-ah-backend',  
       userId: 'U4R9RDUCQ'
     }, {
-      id: Sequelize.UUIDV4,
+      id: Sequelize.Utils.generateUUID(),
       url: 'https://github.com/andela/artemis-ah-backend',  
       userId: 'U4R9RDUCQ'
     }, {
-      id: Sequelize.UUIDV4,
+      id: Sequelize.Utils.generateUUID(),
       url: 'https://github.com/andela/apollo-ah-frontend',  
       userId: 'U4R9RDUCQ'
     }, {
-      id: Sequelize.UUIDV4,
+      id: Sequelize.Utils.generateUUID(),
       url: 'https://github.com/andela/artemis-ah-frontend',  
       userId: 'U4R9RDUCQ'
     }, {
-      id: Sequelize.UUIDV4,
+      id: Sequelize.Utils.generateUUID(),
       url: 'https://www.pivotaltracker.com/n/projects/2313265',  
       userId: 'UBP16GXMX'
     }, {
-      id: Sequelize.UUIDV4,
+      id: Sequelize.Utils.generateUUID(),
       url: 'https://www.pivotaltracker.com/n/projects/2313267',  
       userId: 'UBP16GXMX'
     }], {}),
